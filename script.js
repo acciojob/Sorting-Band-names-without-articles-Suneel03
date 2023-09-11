40f787155e9cd3449a396b69340b2302e87b1202
Suneel Kythapalli <139736258+Suneel03@users.noreply.github.com>
2023-09-11 12:01:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here
+function sortBandNames(bandNames) {
+    return bandNames.sort((a, b) => {
+        // Function to remove articles and leading spaces
+        const removeArticles = (str) => str.replace(/^(the|an|a)\s+/i, '');
+
+        const nameA = removeArticles(a.toLowerCase());
+        const nameB = removeArticles(b.toLowerCase());
+
+        return nameA.localeCompare(nameB);
+    });
+}
+
+// Sort the band names
+bandNames = sortBandNames(bandNames);
+
+// Display the sorted band names in an HTML list
+const ul = document.getElementById('band');
+
+bandNames.forEach((bandName) => {
+    const li = document.createElement('li');
+    li.textContent = bandName;
+    ul.appendChild(li);
+});
 
