e3b0e4a3e45d2193ad29faa21336053105f7507a
Suneel Kythapalli <139736258+Suneel03@users.noreply.github.com>
2023-09-11 12:11:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
 //your code here
-let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-function sortBandNames(bandNames) {
-    return bandNames.sort((a, b) => {
-        // Function to remove articles and leading spaces
-        const removeArticles = (str) => str.replace(/^(the|an|a)\s+/i, '');
 
-        const nameA = removeArticles(a.toLowerCase());
-        const nameB = removeArticles(b.toLowerCase());
-
-        return nameA.localeCompare(nameB);
-    });
-}
-
-// Sort the band names
-bandNames = sortBandNames(bandNames);
-
-// Display the sorted band names in an HTML list
-const ul = document.getElementById('band');
-
-bandNames.forEach((bandName) => {
-    const li = document.createElement('li');
-    li.textContent = bandName;
-    ul.appendChild(li);
-});
 
