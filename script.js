f14638b807f1ccd3d27f33ed842e29fa9a22acf9
Suneel Kythapalli <139736258+Suneel03@users.noreply.github.com>
2023-09-11 12:52:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
 //your code here
-let regexp = /\bthe\b|\ban\b|\ba\b/gi;
-// "$ rockey launched $ $ rocket"
-//console.log(str.replace(regexp, "$"));
-
-let arr =  ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];;
-
-let articleLessArray = [];
-
-let mp = {};
-// {aricleLessItem: ""}
-
-for (let i = 0; i < arr.length; i++) {
-  let articleLessItem = arr[i].replace(regexp, "").trim();
-  articleLessItem = articleLessItem.replace(/  /g, " "); // remove two spaces with a single space
-  articleLessArray.push(articleLessItem);
-  mp[articleLessItem] = arr[i];
-
-}
-
-articleLessArray.sort();
-
-let ans = [];
-
-for (let i of articleLessArray) {
-  ans.push(mp[i]);
-}
-
-console.log(ans);
 
 
